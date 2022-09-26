374d6fa96e6c654d9be7ced15a61a8c41bfa51d6
falcaomm <manu.m.falcao@gmail.com>
2022-09-26 19:55:40 -0300

    ao vivo

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+let sapoVar = document.getElementById("sapo")
+sapoVar.innerHTML += " pé."
+
+let poemaVar = document.getElementById("poema")
+poemaVar.innerHTML += " evidências."
+
+let sabedoriaVar = document.getElementById("sabedoria")
+sabedoriaVar.innerHTML += " voando."
+
+let borboletaVar = document.getElementById("borboleta")
+borboletaVar.innerHTML += " picapau auau."
+
+let robertoVar = document.getElementById("roberto")
+robertoVar.innerHTML += " vivi."
